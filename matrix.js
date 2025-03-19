3291d14e25ee251bdc4b35af15dc9a87e52dc74c
RobloxExecutormaker617 <naseebnoveera0@gmail.com>
2025-03-19 02:27:01 -0700

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -4,38 +4,30 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
-const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const nums = '0123456789';
-const alphabet = katakana + latin + nums;
-
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ12345FUCK6789@#$%^&*()*&^%';
 const fontSize = 16;
 const columns = canvas.width / fontSize;
-const rainDrops = Array(Math.floor(columns)).fill(0);
 
-const draw = () => {
+const drops = Array(Math.floor(columns)).fill(1);
+
+function draw() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = '#0F0';
+  ctx.fillStyle = '#00ff00';
   ctx.font = `${fontSize}px monospace`;
 
-  rainDrops.forEach((y, index) => {
-    const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+  drops.forEach((y, index) => {
+    const text = letters.charAt(Math.floor(Math.random() * letters.length));
     const x = index * fontSize;
-    ctx.fillText(text, x, y);
+    ctx.fillText(text, x, y * fontSize);
 
-    if (y > canvas.height && Math.random() > 0.975) {
-      rainDrops[index] = 0;
-    } else {
-      rainDrops[index] = y + fontSize;
+    if (y * fontSize > canvas.height && Math.random() > 0.975) {
+      drops[index] = 0;
     }
+
+    drops[index]++;
   });
-};
+}
 
-setInterval(draw, 30);
-
-window.addEventListener('resize', () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-});
+setInterval(draw, 33);
